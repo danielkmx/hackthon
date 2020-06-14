13f78dd1269c127bc6021222a4236e66c0951bf0
Daniel Bezerra <daniel.bezerra@somospi.com.br>
2020-06-14 14:39:32 -0300

    novo build

---
+++ build/precache-manifest.51be3419924cb52bd6e701990b20121a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac33dab32a953a6a927fe8ea48e63cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "830564d215f0f854976c",
+    "url": "/static/css/main.27c82ea4.chunk.css"
+  },
+  {
+    "revision": "c42665fafccb039fd68e",
+    "url": "/static/js/2.193ea926.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/static/js/2.193ea926.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "830564d215f0f854976c",
+    "url": "/static/js/main.3c65871d.chunk.js"
+  },
+  {
+    "revision": "5b141301963352c4921f",
+    "url": "/static/js/runtime-main.256c416b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -6,72 +6,38 @@ import Speech from 'react-speech';
 import ChatBot from 'react-simple-chatbot';
 
 function App() {
+  const sleep = (m) => new Promise((r) => setTimeout(r, m));
   const [route, setRoute] = useState([]);
   const [position, setPosition] = useState({});
   const [destination, setDestination] = useState(false);
-  const [etapa, setEtapa] = useState(0);
-  const [mensagens, setMensagens] = useState([]);
   const [shouldSearch, setShouldSearch] = useState(false);
   const [googleTemp, setGoogleTemp] = useState({});
-  const [respostas, setRespostas] = useState([]);
   useEffect(() => {
-    setTimeout(() => {
-      setEtapa(1);
-      setMensagens([{ text: 'Qual destino da sua viagem?' }]);
-    }, 1500);
+    setTimeout(() => {}, 1500);
     setGoogleTemp(window.google);
     navigator.geolocation.getCurrentPosition((pos) =>
       setPosition({ lat: pos.coords.latitude, lng: pos.coords.longitude }),
     );
   });
 
-  const dicionarioPerguntas = {
-    1: 'Qual destino da sua viagem?',
-    2: 'endereco',
-    3: 'Que tipo de local para almoçar você prefere?',
-    4: ['O lugar mais em conta', 'Um lugar com melhor nota'],
-  };
-
   const voice = async () => {
     console.log('------------VOICE--------');
     const synthesis = window.speechSynthesis;
 
-    // Get the first `en` language voice in the list
     var voice = await synthesis.getVoices().filter(function (voice) {
       return voice.lang === 'pt-BR';
     });
     console.log(voice);
-    // Create an utterance object
     var utterance = new SpeechSynthesisUtterance(
       'Voce esta a 2 kilometros da sua parada para comer',
     );
 
-    // Set utterance properties
     utterance.voice = voice[0];
     utterance.pitch = 1.0;
     utterance.rate = 1.0;
     utterance.volume = 0.8;
 
-    // Speak the utterance
     synthesis.speak(utterance);
-  };
-  const addPerguntas = (etapa, mensagens) => {
-    const etapaNova = etapa + 1;
-    setTimeout(() => {
-      setEtapa(etapaNova);
-      setMensagens([...mensagens, { type: 'bot', text: dicionarioPerguntas[etapaNova] }]);
-    }, 1500);
-  };
-  const addResposta = async (respostas, resposta, etapa) => {
-    setTimeout(() => {
-      if (dicionarioPerguntas[etapa] === 'endereco') {
-        setDestination(resposta);
-        setShouldSearch(true);
-      }
-      setRespostas([...respostas, resposta]);
-      setEtapa(etapa + 1);
-      setMensagens([...mensagens, { type: 'bot', text: dicionarioPerguntas[etapa + 1] }]);
-    }, 1500);
   };
 
   return (
@@ -81,26 +47,68 @@ function App() {
         steps={[
           {
             id: '1',
-            component: <div><strong>Oi Renato, eu sou o seu parceiro de bordo, o Chapa!</strong> Eu vou te ajudar a estar no lugar certo, do jeito certo e na hora certa 🙏</div>,
-            trigger: 2,
+            component: (
+              <div>
+                <strong>Oi Renato, eu sou o seu parceiro de bordo, o Chapa!</strong> Eu vou te
+                ajudar a estar no lugar certo, do jeito certo e na hora certa 🙏
+              </div>
+            ),
+            trigger: () => {
+              return 2;
+            },
           },
           {
             id: '2',
-            component: <div>Você pode conta comigo para te ajudar e ver a sua programação completa após definir o destino.</div>,
-            trigger: 3,
+            component: (
+              <div>
+                Você pode conta comigo para te ajudar e ver a sua programação completa após definir
+                o destino.
+              </div>
+            ),
+            trigger: () => {
+              (async () => {
+                console.time('Slept for');
+                await sleep(3000);
+                console.timeEnd('Slept for');
+              })();
+              return 3;
+            },
           },
           {
             id: '3',
-            component: <div>Como estar no lugar certo e na hora certa.
-                      <ul><li>Encontrar <strong>Paradas Seguras</strong> 🅿️ recomendadas.</li>
-                      <li>Saber onde são os <strong>Postos de Gasolina</strong>. ⛽</li>
-                      <li>Comer nos <strong>Restaurantes</strong> 🍛 mais adequados para você.</li></ul></div>,
-            trigger:`3.1`,
+            component: (
+              <div>
+                Como estar no lugar certo e na hora certa.
+                <ul>
+                  <li>
+                    Encontrar <strong>Paradas Seguras</strong> 🅿️ recomendadas.
+                  </li>
+                  <li>
+                    Saber onde são os <strong>Postos de Gasolina</strong>. ⛽
+                  </li>
+                  <li>
+                    Comer nos <strong>Restaurantes</strong> 🍛 mais adequados para você.
+                  </li>
+                </ul>
+              </div>
+            ),
+            trigger: () => {
+              (async () => {
+                console.time('Slept for');
+                await sleep(1000);
+                console.timeEnd('Slept for');
+              })();
+              return '3.1';
+            },
           },
           {
             id: '3.1',
-            component: <div>Antes de começar,
-             eu vou precisar da sua ajuda para definir suas preferências de jornada, são só 5 perguntas. Qual a sua idade <strong>(1/5)</strong>?</div>,
+            component: (
+              <div>
+                Antes de começar, eu vou precisar da sua ajuda para definir suas preferências de
+                jornada, são só 5 perguntas. Qual a sua idade <strong>(1/5)</strong>?
+              </div>
+            ),
             trigger: '4',
           },
           {
@@ -115,7 +123,12 @@ function App() {
 
           {
             id: '5',
-            component: <div>Com qual frequência você gostaria de fazer exercícios durante a semana <strong>(2/5)</strong>?</div>,
+            component: (
+              <div>
+                Com qual frequência você gostaria de fazer exercícios durante a semana{' '}
+                <strong>(2/5)</strong>?
+              </div>
+            ),
             trigger: '6',
           },
           {
@@ -128,24 +141,38 @@ function App() {
           },
           {
             id: '7.1',
-            component: <div>Mandou bem!
-            De forma geral, a atividade física e um fator determinante para maiores benefícios à saúde 💪.
-            Posso te notificar durante o dia para vocês não se esquecer de beber a quantidade de água certa <strong>(3/5)</strong>?</div>,
-            trigger: `7.4`
+            component: (
+              <div>
+                Mandou bem! De forma geral, a atividade física e um fator determinante para maiores
+                benefícios à saúde 💪. Posso te notificar durante o dia para vocês não se esquecer
+                de beber a quantidade de água certa <strong>(3/5)</strong>?
+              </div>
+            ),
+            trigger: `7.4`,
           },
           {
             id: '7.2',
-            component: <div>Parabéns, você está no caminho certo!.
-            De forma geral, a atividade física e um fator determinante para maiores benefícios à saúde 💪.
-            Posso te notificar durante o dia para vocês não se esquecer de beber a quantidade de água certa <strong>(3/5)</strong>?</div>,
-            trigger: `7.4`
+            component: (
+              <div>
+                Parabéns, você está no caminho certo!. De forma geral, a atividade física e um fator
+                determinante para maiores benefícios à saúde 💪. Posso te notificar durante o dia
+                para vocês não se esquecer de beber a quantidade de água certa{' '}
+                <strong>(3/5)</strong>?
+              </div>
+            ),
+            trigger: `7.4`,
           },
           {
             id: '7.3',
-            component: <div><strong>Medalha Silvester Stallonge para você!</strong>
-            De forma geral, a atividade física e um fator determinante para maiores benefícios à saúde 💪.
-            Posso te notificar durante o dia para vocês não se esquecer de beber a quantidade de água certa <strong>(3/5)</strong>?</div>,
-            trigger: `7.4`
+            component: (
+              <div>
+                <strong>Medalha Silvester Stallonge para você!</strong>
+                De forma geral, a atividade física e um fator determinante para maiores benefícios à
+                saúde 💪. Posso te notificar durante o dia para vocês não se esquecer de beber a
+                quantidade de água certa <strong>(3/5)</strong>?
+              </div>
+            ),
+            trigger: `7.4`,
           },
           {
             id: '7.4',
@@ -156,29 +183,51 @@ function App() {
           },
           {
             id: '8.1',
-            component: <div>Manter o corpo hidratado é fundamental.
-            Qual dessas frases você se identifica mais <strong>(4/5)</strong>?</div>,
-            trigger: `9`
+            component: (
+              <div>
+                Manter o corpo hidratado é fundamental. Qual dessas frases você se identifica mais{' '}
+                <strong>(4/5)</strong>?
+              </div>
+            ),
+            trigger: `9`,
           },
           {
             id: '8.2',
-            component: <div>Manter o corpo hidratado é fundamental. Se mudar de ideia é só alterar as preferências em seu perfil.
-            Qual dessas frases você se identifica mais <strong>(4/5)</strong>?</div>,
-            trigger: `9`
+            component: (
+              <div>
+                Manter o corpo hidratado é fundamental. Se mudar de ideia é só alterar as
+                preferências em seu perfil. Qual dessas frases você se identifica mais{' '}
+                <strong>(4/5)</strong>?
+              </div>
+            ),
+            trigger: `9`,
           },
           {
             id: '9',
             options: [
-              { value: 10, label: '(A) Pequenos barulhos ou um pouco de luz me mantem acordado ou me acordam com facilidade.', trigger: '10' },
-              { value: 11, label: '(B) Eu normalmente acordo antes do meu dispertador tocar.', trigger: '10' },
+              {
+                value: 10,
+                label:
+                  '(A) Pequenos barulhos ou um pouco de luz me mantem acordado ou me acordam com facilidade.',
+                trigger: '10',
+              },
+              {
+                value: 11,
+                label: '(B) Eu normalmente acordo antes do meu dispertador tocar.',
+                trigger: '10',
+              },
               { value: 12, label: '(C) Eu já fui diagnóstica com insonia.', trigger: '10' },
               { value: 13, label: '(D) Sinto cansaço após o almoço.', trigger: '10' },
             ],
           },
           {
             id: '10',
-            component: <div>Qual é sua preferência <strong>(5/5)</strong>?</div>,
-            trigger: `11`
+            component: (
+              <div>
+                Qual é sua preferência <strong>(5/5)</strong>?
+              </div>
+            ),
+            trigger: `11`,
           },
           {
             id: '11',
@@ -195,11 +244,15 @@ function App() {
           },
           {
             id: '12',
-            component: <div>Tudo anotado aqui, <strong>vamos botar o pé na estrada!</strong> 🚛💨</div>,
-            end:true
-          },            
+            component: (
+              <div>
+                Tudo anotado aqui, <strong>vamos botar o pé na estrada!</strong> 🚛💨
+              </div>
+            ),
+            end: true,
+          },
         ]}
-      />  
+      />
       <Map
         setShouldSearch={setShouldSearch}
         shouldSearch={shouldSearch}
